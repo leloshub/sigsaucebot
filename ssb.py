c8dc0b0a6c08f657104b33ef9154e7929d379b65
leloshub <hussleperseverance@gmail.com>
2026-05-05 08:09:18 -0400

    ssb.py

--- ssb.py
+++ ssb.py
@@ -47,7 +47,7 @@ MAX_HISTORY  = 10
 
 def load_config():
     try:
-        with. open(CONFIG_FILE) as f:
+        with open(CONFIG_FILE) as f:
             return json.load(f)
     except Exception:
         return {“balance”: None, “risk_pct”: 1.0}
